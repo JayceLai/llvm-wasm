a72f764b0043027f37b5149f0486915e331e8a3b
JayceLai <goodlaijiaxin@qq.com>
2020-12-12 18:37:39 +0800

    [extenal] add binaryen

--- extenal/env.sh
+++ extenal/env.sh
@@ -5,7 +5,10 @@ extenalFolder=$(
     pwd
 )
 echo "EXTENAL-DIR: "${extenalFolder}
-export PATH=$PATH:${extenalFolder}/wasi-sdk/bin
-export PATH=$PATH:${extenalFolder}/wasi-sdk/bin
+
+export PATH=${extenalFolder}/wasi-sdk/bin:$PATH
+export PATH=${extenalFolder}/wasi-sdk/bin:$PATH
 export CPLUS_INCLUDE_PATH=${extenalFolder}/wasi-sdk/share/wasi-sysroot/include:$CPLUS_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=${extenalFolder}/wasi-sdk/share/wasi-sysroot/include/c++/v1:$CPLUS_INCLUDE_PATH
+
+export PATH=${extenalFolder}/binaryen/bin:$PATH
